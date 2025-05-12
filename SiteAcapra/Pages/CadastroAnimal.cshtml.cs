5dae1125a141bc8a03afd71beabcd13cb73d1a94
Wellington <wellingtondacunha2004@gmail.com>
2025-05-12 20:38:36 -0300

    Mescla com master

---
+++ SiteAcapra/Pages/CadastroAnimal.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SiteAcapra.Pages
+{
+    public class CadastroAnimalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
