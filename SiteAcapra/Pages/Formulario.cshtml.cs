5375727ea4ab35f1222204413444c0ba1ec52a81
iagog <iagogeovanesalvador@gmail.com>
2025-05-12 20:11:42 -0300

    formulario criado

---
+++ SiteAcapra/Pages/Formulario.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SiteAcapra.Pages
+{
+    public class FormularioModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
