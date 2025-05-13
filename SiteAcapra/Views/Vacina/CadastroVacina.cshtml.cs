b1ae4de4f2145f2e5c06dcc62539542508caff42
Wellington <wellingtondacunha2004@gmail.com>
2025-05-12 21:43:15 -0300

    Criação das Páginas CadastroAnimal e CadastroVacina

---
+++ SiteAcapra/Views/Vacina/CadastroVacina.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SiteAcapra.Views.Vacina
+{
+    public class CadastroVacinaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
